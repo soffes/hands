eb9bcc66db91a1aafde25ccf489c6beaf130376f
Sam Soffes <sam@samsoff.es>
2011-12-23 00:46:30 -0500

    Clean up a bit

--- lib/hands/card.rb
+++ lib/hands/card.rb
@@ -1,41 +1,53 @@
 module Hands
   class Card
     include Comparable
-    
+
     attr_accessor :suite
     attr_accessor :value
-    
+
     def self.[](first = nil, second = nil)
       self.new(first, second)
     end
-    
+
     def initialize(first = nil, suite = nil)
+      # Value provided
       if first.is_a?(Integer) or first.is_a?(String)
         self.value = first
+
+      # Hash provided
       elsif first.is_a?(Hash)
         self.value = first[:value] if first[:value]
         self.suite = first[:suite] if first[:suite]
       end
       
+      # Set suite
       self.suite = suite if suite
     end
-    
+
     def value=(val)
+      # Integer
       if val.is_a?(Integer)
+        # Number range
         if val > 0 and val <= 10
           @value = val
           return
+
+        # Face card or ace range
         elsif val > 10 and val < 15
           @value = VALUES[val - 2]
           return
         end
+
+      # String
       elsif val.is_a?(String) and VALUES.include?(val.downcase)
         @value = val.downcase
         return
       end
+
+      # Invalid
       @value = nil
     end
-    
+
     def inspect
       if self.is_valid?
         "#{super} #{self.description}"
@@ -43,15 +55,15 @@ module Hands
         super
       end
     end
-    
+
     def is_valid?
       SUITES.include?(self.suite.to_s) and VALUES.include?(self.value.to_s.downcase)
     end
-    
+
     def is_invalid?
-      !self.is_valid?      
+      !self.is_valid?
     end
-    
+
     def description
       if self.is_valid?
         "#{VALUE_DESCRIPTIONS[self.value_index].capitalize} of #{self.suite.to_s.capitalize}"
@@ -59,18 +71,18 @@ module Hands
         'invalid'
       end
     end
-    
+
     def <=>(other_card, check_suite = false)
       # TODO: Handle invalid cards
       result = self.value_index <=> other_card.value_index
       return self.suite_index <=> other_card.suite_index if result == 0 and check_suite
       result
     end
-    
+
     def suite_index
       SUITES.index(self.suite.to_s.downcase)
     end
-    
+
     def value_index
       VALUES.index(self.value.to_s.downcase)
     end
--- lib/hands/hand.rb
+++ lib/hands/hand.rb
@@ -1,30 +1,32 @@
 module Hands
   class Hand
     include Comparable
-    
+
     attr_accessor :cards
-    
+
     def cards
       @cards ||= []
     end
         
     def <=>(other_hand)
       response = (self.hand_index <=> other_hand.hand_index)
+
+      # If the hands tie, see which is higher (i.e. higher pair)
       if response == 0
         self.cards <=> other_hand.cards
       else
         response
       end
     end
-    
+
     def <<(card)
       self.cards << card
     end
-    
+
     def suites
       self.cards.collect(&:suite).uniq
     end
-    
+
     def best_hand
       response = {}
       HAND_ORDER.reverse.each do |type|
@@ -36,46 +38,46 @@ module Hands
       end
       response
     end
-    
+
     def high_card
       self.cards.sort.reverse
     end
-    
+
     def pair
       self.pairs(1)
     end
-    
+
     def two_pair
       self.pairs(2)
     end
-    
+
     def three_of_a_kind
       nil
     end
-    
+
     def straight
       return nil unless self.cards.length == 5
       cs = self.cards.sort.reverse
-      
+
       # Ace's low
       if cs.first.value == 'a' and cs[1].value == 5
         # Move ace to end
         ace = cs.first
         cs = cs[1..4]
         cs << ace
-        
+
         # Check succession
         csr = cs.reverse
         4.times do |i|
           next if i == 0
           return nil unless csr[i].value_index == i - 1
         end
-      
+
       # Normal
       else
         # Check range
         return nil unless cs.first.value_index - cs.last.value_index == 4
-      
+
         # Check succession
         4.times do |i|        
           return nil unless cs[i].value_index == cs[i + 1].value_index + 1
@@ -85,43 +87,47 @@ module Hands
     end
     
     def flush
+      # If all of the cards are the same suite, we have a flush
       return nil unless self.suites.length == 1
       self.cards.sort.reverse
     end
     
     def full_house
+      # TODO: Implement
       nil
     end
     
-    def four_of_a_kind    
-      nil  
+    def four_of_a_kind
+      # TODO: Implement
+      nil
     end
     
     def straight_flush
-      nil      
+      # TODO: Implement
+      nil
     end
     
     protected
-    
+
     def hand_index
       best = self.best_hand
       return -1 if best.nil?
       HAND_ORDER.index(best[:type].to_s)
     end
-    
+
     def duplicates
       pairs = self.cards.collect(&:value)
       pairs.uniq.select{ |e| (pairs - [e]).size < pairs.size - 1 }
     end
-    
+
     def pairs(min)
       dupes = self.duplicates
       return nil if dupes.length < min
-      
+
       hand = self.cards.select do |card|
         dupes.include?(card.value)
       end
-      
+
       hand = hand.sort.reverse
       hand << (self.cards - hand).sort.reverse
       hand.flatten
